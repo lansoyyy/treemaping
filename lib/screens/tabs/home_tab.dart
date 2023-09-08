9cc75774c0a4057d5a95f4f4ac118e72e763909f
Lans12345 <olanalans12345@gmail.com>
2023-09-08 18:05:34 +0800

    added map ui

--- lib/screens/tabs/home_tab.dart
+++ lib/screens/tabs/home_tab.dart
@@ -1,17 +1,140 @@
+import 'dart:collection';
+
+import 'package:communal/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-import '../../utils/colors.dart';
-import '../../widgets/button_widget.dart';
-import '../../widgets/text_widget.dart';
-
-class HomeTab extends StatelessWidget {
+class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
 
   @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  GoogleMapController? mapController;
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  late double lat;
+  late double long;
+  Set<Polygon> polygon = HashSet<Polygon>();
+
+  @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      height: 400,
-      width: 500,
+    CameraPosition initialCameraPosition = const CameraPosition(
+      target: LatLng(8.332155, 124.975465),
+      zoom: 14,
+    );
+    return Stack(
+      children: [
+        GoogleMap(
+          zoomControlsEnabled: false,
+          mapType: MapType.hybrid,
+          polygons: polygon,
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: initialCameraPosition,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 50, right: 20, top: 50),
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: Card(
+              color: Colors.white.withOpacity(0.5),
+              child: SizedBox(
+                height: 700,
+                width: 300,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(15, 20, 15, 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      TextWidget(
+                        text: 'Endimic Tree Records',
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontFamily: 'Bold',
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  width: double.infinity,
+                                  height: 75,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    border: Border.all(color: Colors.black),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 10, right: 10),
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Container(
+                                          color: Colors.grey,
+                                          height: 50,
+                                          width: 50,
+                                        ),
+                                        const SizedBox(
+                                          width: 20,
+                                        ),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            TextWidget(
+                                              text: 'Name of Tree',
+                                              fontSize: 16,
+                                              fontFamily: 'Bold',
+                                              color: Colors.black,
+                                            ),
+                                            const SizedBox(
+                                              height: 5,
+                                            ),
+                                            TextWidget(
+                                              text: 'Description of Tree',
+                                              fontSize: 12,
+                                              fontFamily: 'Medium',
+                                              color: Colors.black,
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }

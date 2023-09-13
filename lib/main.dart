0bda85066833040972b9f7d313025c7734cf17e0
Lans12345 <olanalans12345@gmail.com>
2023-09-13 17:48:21 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:communal/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          authDomain: 'treemaping-45201.firebaseapp.com',
+          apiKey: "AIzaSyBpPh8yBpqzVkt6Q0JNPtjWrR-osV7TdE4",
+          appId: "1:988715309097:web:69a45d17f9f162718d6438",
+          messagingSenderId: "988715309097",
+          projectId: "treemaping-45201",
+          storageBucket: "treemaping-45201.appspot.com"));
   runApp(const MyApp());
 }
 

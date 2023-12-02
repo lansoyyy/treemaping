eb0643bd5a33feeda28a327d5f5ddb4e70fda864
Lans12345 <olanalans12345@gmail.com>
2023-12-02 08:08:01 +0800

    fixed login

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -64,69 +64,128 @@ class _HomeScreenState extends State<HomeScreen> {
                             ],
                     ),
                   ),
-                  IconButton(
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: TextWidget(
-                              text: 'Enter Admin Credentails',
-                              fontSize: 18,
-                              fontFamily: 'Bold',
-                            ),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                SizedBox(
-                                  height: 100,
-                                  child: TextFieldWidget(
-                                      label: 'Username',
-                                      controller: usernameController),
-                                ),
-                                SizedBox(
-                                  height: 100,
-                                  child: TextFieldWidget(
-                                      label: 'Password',
-                                      showEye: true,
-                                      isObscure: true,
-                                      controller: passController),
-                                ),
-                              ],
-                            ),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  if (passController.text != 'admin-username' &&
-                                      passController.text != 'admin-password') {
-                                    Navigator.pop(context);
-                                    showToast('Incorrect admin password!');
-                                  } else {
-                                    Navigator.of(context)
-                                        .pushReplacement(MaterialPageRoute(
-                                            builder: (context) => HomeScreen(
-                                                  inUser: false,
-                                                )));
+                  widget.inUser!
+                      ? IconButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: TextWidget(
+                                    text: 'Enter Admin Credentails',
+                                    fontSize: 18,
+                                    fontFamily: 'Bold',
+                                  ),
+                                  content: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      SizedBox(
+                                        height: 100,
+                                        child: TextFieldWidget(
+                                            label: 'Username',
+                                            controller: usernameController),
+                                      ),
+                                      SizedBox(
+                                        height: 100,
+                                        child: TextFieldWidget(
+                                            label: 'Password',
+                                            showEye: true,
+                                            isObscure: true,
+                                            controller: passController),
+                                      ),
+                                    ],
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        if (passController.text !=
+                                                'admin-username' &&
+                                            passController.text !=
+                                                'admin-password') {
+                                          Navigator.pop(context);
+                                          showToast(
+                                              'Incorrect admin password!');
+                                        } else {
+                                          Navigator.of(context).pushReplacement(
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      HomeScreen(
+                                                        inUser: false,
+                                                      )));
 
-                                    showToast('Logged in as admin!');
-                                  }
-                                },
-                                child: TextWidget(
-                                  text: 'Continue',
-                                  fontSize: 18,
-                                  fontFamily: 'Bold',
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    },
-                    icon: const Icon(
-                      Icons.admin_panel_settings,
-                    ),
-                  ),
+                                          showToast('Logged in as admin!');
+                                        }
+                                      },
+                                      child: TextWidget(
+                                        text: 'Continue',
+                                        fontSize: 18,
+                                        fontFamily: 'Bold',
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                          icon: const Icon(
+                            Icons.admin_panel_settings,
+                          ),
+                        )
+                      : const SizedBox(),
+                  !widget.inUser!
+                      ? IconButton(
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                      title: const Text(
+                                        'Logout Confirmation',
+                                        style: TextStyle(
+                                            fontFamily: 'QBold',
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      content: const Text(
+                                        'Are you sure you want to Logout?',
+                                        style:
+                                            TextStyle(fontFamily: 'QRegular'),
+                                      ),
+                                      actions: <Widget>[
+                                        MaterialButton(
+                                          onPressed: () =>
+                                              Navigator.of(context).pop(true),
+                                          child: const Text(
+                                            'Close',
+                                            style: TextStyle(
+                                                fontFamily: 'QRegular',
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                        MaterialButton(
+                                          onPressed: () async {
+                                            Navigator.of(context)
+                                                .pushReplacement(
+                                                    MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            HomeScreen(
+                                                              inUser: true,
+                                                            )));
+                                          },
+                                          child: const Text(
+                                            'Continue',
+                                            style: TextStyle(
+                                                fontFamily: 'QRegular',
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                      ],
+                                    ));
+                          },
+                          icon: const Icon(
+                            Icons.logout,
+                          ),
+                        )
+                      : const SizedBox()
                 ],
               ),
               // Row(
--- lib/screens/tabs/aboutus_tab.dart
+++ lib/screens/tabs/aboutus_tab.dart
@@ -9,79 +9,105 @@ class AboutUsTab extends StatelessWidget {
     return Center(
       child: Container(
         padding: const EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Image.asset(
-                      'assets/images/logs.png',
-                      height: 150,
-                      width: 250,
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      'Our Company',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    const Text(
-                      'The greatest wealth is a healty planet',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      'Our Mission',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    const Text(
-                      'To create innovative solutions that make a difference.',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      'Contact Us',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    const Text(
-                      'Email: mariobaldo2001@gmail.com\nPhone: +1 (123) 456-7890',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 1000,
+                height: 300,
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/back.jpg'),
+                      opacity: 0.25,
+                      fit: BoxFit.cover),
+                ),
+                child: Center(
+                  child: TextWidget(
+                    text: 'About Us',
+                    fontSize: 32,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              width: 300,
-              child: TextWidget(
-                text:
-                    'Kisolon Tree Park, nestled in the heart of Brgy. Kisolon, Sumilao, Bukidnon, is a natural wonderland celebrated for its thriving biodiversity and plethora of native tree species. The park sprawls across rolling hills, dense woodlands, and pristine streams, providing a haven for tree species like Narra, Tanguile, Molave, Almond, Lauan, Tipolo, and Tulips, each uniquely adapted to its local habitat. Beyond its ecological significance, Kisolon Tree Park stands as a testament to the enduring power of community-led conservation efforts. Local government agencies, non-profit organizations, and community groups collaborate diligently to execute sustainable reforestation practices, monitor wildlife populations, and bolster environmental education initiatives. In addition to its role in conservation, the park beckons eco-enthusiasts and nature lovers with its extensive trail network, scenic beauty, and opportunities for bird-watching and wildlife observation. It also holds a profound cultural and historical value, serving as a sacred site where indigenous traditions and ecological wisdom are lovingly preserved. Kisolon Tree Park represents a powerful call to action for the preservation and rejuvenation of our forests, fostering the well-being of both the environment and future generations.',
-                fontSize: 14,
-                color: Colors.grey,
-                fontFamily: 'Regular',
+              const SizedBox(
+                height: 20,
               ),
-            ),
-          ],
+              SizedBox(
+                width: 1000,
+                child: TextWidget(
+                  text:
+                      'Kisolon Tree Park, nestled in the heart of Brgy. Kisolon, Sumilao, Bukidnon, is a natural wonderland celebrated for its thriving biodiversity and plethora of native tree species. The park sprawls across rolling hills, dense woodlands, and pristine streams, providing a haven for tree species like Narra, Tanguile, Molave, Almond, Lauan, Tipolo, and Tulips, each uniquely adapted to its local habitat. Beyond its ecological significance, Kisolon Tree Park stands as a testament to the enduring power of community-led conservation efforts. Local government agencies, non-profit organizations, and community groups collaborate diligently to execute sustainable reforestation practices, monitor wildlife populations, and bolster environmental education initiatives. In addition to its role in conservation, the park beckons eco-enthusiasts and nature lovers with its extensive trail network, scenic beauty, and opportunities for bird-watching and wildlife observation. It also holds a profound cultural and historical value, serving as a sacred site where indigenous traditions and ecological wisdom are lovingly preserved. Kisolon Tree Park represents a powerful call to action for the preservation and rejuvenation of our forests, fostering the well-being of both the environment and future generations.',
+                  fontSize: 14,
+                  color: Colors.grey,
+                  fontFamily: 'Regular',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset(
+                        'assets/images/logs.png',
+                        height: 150,
+                        width: 250,
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        'Our Company',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'The greatest wealth is a healty planet',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        'Our Mission',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'To create innovative solutions that make a difference.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        'Contact Us',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Email: mariobaldo2001@gmail.com\nPhone: +1 (123) 456-7890',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
